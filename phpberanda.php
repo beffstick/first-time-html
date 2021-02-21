7aec0a199c3ffcda1831ca937ae2d3d37bc4e509
beffstick <79389790+beffstick@users.noreply.github.com>
2021-02-21 11:45:12 +0800

    Add files via upload

---
+++ phpberanda.php
@@ -0,0 +1,206 @@
+﻿<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="cssberanda.css">
+<head>
+<title>DolanKomputer</title>
+<link rel="icon" href="/htmltes/img/ikon.png" type="image/x-icon"> 
+<h1>Dolan Komputer</h1>
+</head>
+<div class="topnav">
+  <a href="/htmltes/htmlberanda.html"><b>Beranda | </b></a>
+  <a href="/htmltes/daftarartikel.html"><b>Daftar Artikel | </b></a>
+  <a href="/htmltes/aboutwebsite.html"><b>About Website | </b></a>
+  <a><input type="text" placeholder="Cari Artikel"><button>Cari</button></a>
+</div>
+<body>
+<a href="/htmltes/htmlberanda.html" style="font-size: 16pt"><b>Postingan Artikel Terbaru</b></a>
+<br>
+	<div class="column">
+		<div class="card">
+<h3 align="middle"><a href="/htmltes/htmlartikel.html">Fix Virus Shortcut di Flashdisk</a></h3>
+<div align="middle" class="fakeimg" style="height:80px;"><img src="img/shourtcutvirusfix.png" style="width:250px;height:90px;border-radius:8px"></div>
+<div class="a">
+<p style="text-align=center;">Cara fix virus shortcut di flashdisk</p>
+</div>
+</div>
+</div>
+<div class="column">
+<div class="card">
+<h3 align="middle">Artikel</h3>
+<div align="middle"class="fakeimg" style="height:80px;"><img src="img/tenor2.gif" style="width:250px;height:90px;border-radius:8px"></div>
+<div class="b">
+<p>nantilah kalau ada artikel</p>
+</div>
+</div>
+</div>
+<div class="column">
+<div class="card">
+<h3 align="middle">Artikel</h3>
+<div align="middle"class="fakeimg" style="height:80px;"><img src="img/tenor.gif" style="width:250px;height:90px;border-radius:8px"></div>
+<div class="b">
+<p>nantilah kalau ada artikel</p>
+</div>
+</div>
+</div>
+<div class="column">
+		<div class="card">
+<h3 align="middle">Artikel</h3>
+<div align="middle" class="fakeimg" style="height:80px;"><img src="img/tenor1.gif" style="width:250px;height:90px;border-radius:8px"></div>
+<div class="a">
+<p style="text-align=center;">nantilah kalau ada artikel</p>
+</div>
+</div>
+</div>
+<a href="/htmltes/htmlberanda.html" style="font-size: 16pt"><b>Populer</b></a>
+<br>
+<div class="column">
+<div class="card">
+<h3 align="middle">Artikel</h3>
+<div align="middle"class="fakeimg" style="height:80px;"><img src="img/giphy5.gif" style="width:250px;height:90px;border-radius:8px"></div>
+<div class="b">
+<p>nantilah kalau ada artikel</p>
+</div>
+</div>
+</div>
+<div class="column">
+<div class="card">
+<h3 align="middle">Artikel</h3>
+<div align="middle"class="fakeimg" style="height:80px;"><img src="img/giphy6.gif" style="width:250px;height:90px;border-radius:8px"></div>
+<div class="b">
+<p>nantilah kalau ada artikel</p>
+</div>
+</div>
+</div>
+<div class="column">
+		<div class="card">
+<h3 align="middle">Artikel</h3>
+<div align="middle" class="fakeimg" style="height:80px;"><img src="img/giphy2.gif" style="width:250px;height:90px;border-radius:8px"></div>
+<div class="a">
+<p style="text-align=center;">nantilah kalau ada artikel</p>
+</div>
+</div>
+</div>
+<div class="column">
+<div class="card">
+<h3 align="middle">Artikel</h3>
+<div align="middle"class="fakeimg" style="height:80px;"><img src="img/giphy8.gif" style="width:250px;height:90px;border-radius:8px"></div>
+<div class="b">
+<p>nantilah kalau ada artikel</p>
+</div>
+</div>
+</div>
+<div class="column">
+<div class="card">
+<h3 align="middle">Artikel</h3>
+<div align="middle"class="fakeimg" style="height:80px;"><img src="img/giphy9.gif" style="width:250px;height:90px;border-radius:8px"></div>
+<div class="b">
+<p>nantilah kalau ada artikel</p>
+</div>
+</div>
+</div>
+<div class="column">
+		<div class="card">
+<h3 align="middle">Artikel</h3>
+<div align="middle" class="fakeimg" style="height:80px;"><img src="img/giphy3.gif" style="width:250px;height:90px;border-radius:8px"></div>
+<div class="a">
+<p style="text-align=center;">nantilah kalau ada artikel</p>
+</div>
+</div>
+</div>
+<div class="column">
+<div class="card">
+<h3 align="middle">Artikel</h3>
+<div align="middle"class="fakeimg" style="height:80px;"><img src="img/giphy4.gif" style="width:250px;height:90px;border-radius:8px"></div>
+<div class="b">
+<p>nantilah kalau ada artikel</p>
+</div>
+</div>
+</div>
+<div class="column">
+<div class="card">
+<h3 align="middle">Artikel</h3>
+<div align="middle"class="fakeimg" style="height:80px;"><img src="img/giphy7.gif" style="width:250px;height:90px;border-radius:8px"></div>
+<div class="b">
+<p>nantilah kalau ada artikel</p>
+</div>
+</div>
+</div>
+<a href="/htmltes/htmlberanda.html" style="font-size: 16pt"><b>Curahan Hati Penulis (biasanya habis nonton anime atau baca manga)</b></a>
+<br>
+<div class="column1">
+		<div class="card1">
+<h3 align="middle">One-Punch Man<br>10/10</h3>
+<div align="middle" class="fakeimg" style="height:80px;"><img src="img/opm.gif" style="width:180px;height:90px;border-radius:8px"></div>
+<div class="a">
+<p style="text-align=center;">SAITAMA! SAITAMA! SAITAMA!<br>________</p>
+</div>
+</div>
+</div>
+<div class="column1">
+<div class="card1">
+<h3 align="middle">Kimi no Na wa.<br>9/10</h3>
+<div align="middle"class="fakeimg" style="height:80px;"><img src="img/tenor4.gif" style="width:180px;height:90px;border-radius:8px"></div>
+<div class="b">
+<p>boleh lah<br><br>_________</p>
+</div>
+</div>
+</div>
+<div class="column1">
+<div class="card1">
+<h3 align="middle">Anohana<br>7/10</h3>
+<div align="middle"class="fakeimg" style="height:80px;"><img src="img/tenor5.gif" style="width:180px;height:90px;border-radius:8px"></div>
+<div class="b">
+<p>agak sedih gaes<br><br>_______________</p>
+</div>
+</div>
+</div>
+<div class="column1">
+<div class="card1">
+<h3 align="middle">Koe no Katachi<br>9/10</h3>
+<div align="middle"class="fakeimg" style="height:80px;"><img src="img/giphy10.gif" style="width:180px;height:90px;border-radius:8px"></div>
+<div class="b">
+<p>sedih lah gaes filmnya (kata temen). saya baca manganya sih</p>
+</div>
+</div>
+</div>
+<div class="column1">
+<div class="card1">
+<h3 align="middle">Gekkan Shoujo Nozaki-kun | 8/10</h3>
+<div align="middle"class="fakeimg" style="height:80px;"><img src="img/giphy11.gif" style="width:180px;height:90px;border-radius:8px"></div>
+<div class="b">
+<p>bagus aja komedinya. ada romance sedikit<br>_______________</p>
+</div>
+</div>
+</div>
+<div class="column1">
+<div class="card1">
+<h3 align="middle">Hyouka<br>9/10</h3>
+<div align="middle"class="fakeimg" style="height:80px;"><img src="img/giphy13.gif" style="width:180px;height:90px;border-radius:8px"></div>
+<div class="b">
+<p>bagus<br><br>_____</p>
+</div>
+</div>
+</div>
+</div>
+</body>
+<div class="column2">
+<div class="card2">
+<form>
+Username:<br><input type="text" name="username"><br>
+Password:<br><input type="text" name="username"><br><br>
+<input type="submit" value="Masuk"> | <input type="button" value="Daftar" onclick="window.location.href='htmldaftar.html'"<br><br>
+Comment:<br><textarea rows="7" cols="85" name="comment" form="usrform">Komentar atau Request</textarea>
+<center><input type="submit" value="Submit"></center>
+</form>
+</div>
+</div>
+<footer style= "color blue">
+<div class="c">  
+<p style="color: #e2e77e; font-size: 12px">Dibuat oleh: Satria Fikri Hidayatullah
+<br>
+  Kontak: <a href="mailto:satriafikrihidayatullah2000@gmail.com">
+  Kirim Email Ke Pembuat</a></p>
+</div>
+</footer>
+
+</html>
